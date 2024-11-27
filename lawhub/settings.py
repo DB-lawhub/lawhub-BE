de1df9bc268579cf3870e9a7309807edac8409c8
chaehyeo-n <parkche03@naver.com>
2024-11-27 15:15:53 +0900

    ✨ Feat: app 생성

--- lawhub/settings.py
+++ lawhub/settings.py
@@ -46,6 +46,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'corsheaders',
+    'accounts',
+    'business',
+    'employee',
+    'tax',
 ]
 
 MIDDLEWARE = [
