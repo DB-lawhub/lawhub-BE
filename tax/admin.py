ff31560989b354bceafe0e8057dd9e3db36f923a
chaehyeo-n <parkche03@naver.com>
2024-11-28 17:21:42 +0900

    ✨ Feat: 모델 admin 등록

--- tax/admin.py
+++ tax/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(VAT)
+admin.site.register(IncomeTax)
+admin.site.register(CorporateTax)
+admin.site.register(Rental)
