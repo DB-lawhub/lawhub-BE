79d460a02b5c6b944adffdd0c718b112f1ac5fb1
chaehyeo-n <parkche03@naver.com>
2024-12-01 01:57:21 +0900

    🐛 Fix: Mysql 연동

--- lawhub/settings.py
+++ lawhub/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 import os
 import environ
+import pymysql
+pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -90,8 +92,12 @@ WSGI_APPLICATION = 'lawhub.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'lawhub',
+        'USER' : 'lawhub',
+        'PASSWORD' : os.environ.get('DB_PASSWORD'),
+        'HOST' : os.environ.get('DB_HOST'),
+        'PORT' : '3306',
     }
 }
 
